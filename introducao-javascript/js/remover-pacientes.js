e6fc28a539b9df819ae7d59cc2ac88c3b3a7f731
fenixmiranda <fenixacmiranda@gmail.com>
2023-03-04 18:10:24 -0300

    Remoção, Delegação e Animação do paciente

--- introducao-javascript/js/remover-pacientes.js
+++ introducao-javascript/js/remover-pacientes.js
@@ -1,8 +1,11 @@
 var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        console.log("Fui clicado com um duplo click");
-        this.remove();
-    });
+tabela.addEventListener("dblclick", function (event) {
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function (){
+        event.target.parentNode.remove();
+    }, 500);
+
 });
